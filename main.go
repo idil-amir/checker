ebdfa63816edbf6e196b18627ec77dde8f8f0b55
Muhammad Idil Haq Amir <idil.amir@tokopedia.com>
2023-07-31 10:41:12 +0700

    Update flow

--- main.go
+++ main.go
@@ -9,44 +9,58 @@ import (
 )
 
 var (
-	file1                   = "files/input/file1.csv"
-	file2                   = "files/input/file2.csv"
-	outputFile              = "files/output/non_existing_rows.csv"
-	nonClaimedQuestUserFile = "files/output/non_claimed_quest_user.csv"
+	sourceFile    = "files/input/source.csv"
+	targetFile    = "files/input/target.csv"
+	failedFile    = "files/output/failed.csv"
+	unclaimedFile = "files/output/unclaimed.csv"
 )
 
 func main() {
-
-	// Read the first CSV file
-	rows1, err := readCSVFile(file1)
+	// Read the source CSV file
+	source, err := readCSVFile(sourceFile)
 	if err != nil {
-		fmt.Println("Error reading file1:", err)
+		fmt.Println("Error reading source:", err)
 		return
 	}
 
-	// Read the second CSV file
-	rows2, err := readCSVFile(file2)
+	// Read the target CSV file
+	target, err := readCSVFile(targetFile)
 	if err != nil {
-		fmt.Println("Error reading file2:", err)
+		fmt.Println("Error reading target:", err)
 		return
 	}
 
-	// Get non-existing rows in file2 compared to file1
-	nonExistingRows, nonClaimedQuestUser := getNonExistingRows(rows1, rows2)
+	// Get non-existing rows in target compared to source
+	failedData, unclaimedData := getUnmatchedEntries(source, target)
 
-	if len(nonExistingRows) == 0 {
-		fmt.Println("All rows in file1 exist in file2.")
-	} else {
-		fmt.Println("================Summary================")
-		fmt.Printf("# of Failed Injection : %d", len(nonExistingRows)-1)
-		fmt.Println()
-		fmt.Printf("# of Not Claimed Medali : %d", len(nonClaimedQuestUser)-1)
-		fmt.Println()
-		err := writeNonExistingRowsToFile(nonExistingRows, outputFile)
+	// Print to the output files if there any unmatched entries
+	fmt.Println()
+	fmt.Println("================Logs================")
+	if len(failedData) > 1 {
+		err = writeDataToFile(failedData, failedFile)
 		if err != nil {
 			fmt.Println("Error writing non-existing rows to file:", err)
 		}
+	} else {
+		fmt.Println("No Failed Data")
 	}
+
+	if len(unclaimedData) > 1 {
+		err = writeDataToFile(unclaimedData, unclaimedFile)
+		if err != nil {
+			fmt.Println("Error writing non-existing rows to file:", err)
+		}
+	} else {
+		fmt.Println("No Unclaimed Data")
+	}
+	fmt.Println("================End of Logs================")
+	fmt.Println()
+	fmt.Println("==================Summary==================")
+	fmt.Printf("# of Failed Injection : %d", len(failedData)-1)
+	fmt.Println()
+	fmt.Printf("# of Unclaimed Medali : %d", len(unclaimedData)-1)
+	fmt.Println()
+	fmt.Println("===============End of Summary==============")
 }
 
 // ReadCSVFile reads a CSV file and returns its rows
@@ -66,48 +80,44 @@ func readCSVFile(filename string) ([][]string, error) {
 	return rows, nil
 }
 
-// GetNonExistingRows returns rows in rows1 that do not exist in rows2
-func getNonExistingRows(rows1, rows2 [][]string) (nonExistingRows, nonClaimedQuestUserData [][]string) {
-	existingRows := make(map[string]bool)
+// Getfailed returns rows in source that do not exist in target
+func getUnmatchedEntries(source, target [][]string) (failed, unclaimed [][]string) {
+	existingRows := make(map[string]string)
 
-	// Add all rows in rows2 to the existingRows map
-	for i, row := range rows2 {
+	// Add all rows in target to the existingRows map
+	for _, row := range target {
 		newRow := []string{row[0], row[1]}
 		key := strings.Join(newRow, ",")
-		existingRows[key] = true
-		if i == 0 {
-			nonClaimedQuestUserData = append(nonClaimedQuestUserData, row)
-		}
-		status, _ := strconv.Atoi(row[2])
-		if status == 2 {
-			nonClaimedQuestUserData = append(nonClaimedQuestUserData, row)
-		}
+		existingRows[key] = row[2]
 	}
 
-	err := writeNonExistingRowsToFile(nonClaimedQuestUserData, nonClaimedQuestUserFile)
-	if err != nil {
-		fmt.Println("Error writing non-existing rows to file:", err)
-	}
-
-	// Check if each row in rows1 exists in the existingRows map
-	for i, row := range rows1 {
+	// Check if each row in source exists in the existingRows map
+	for i, row := range source {
 		// header
 		if i == 0 {
-			nonExistingRows = append(nonExistingRows, row)
+			failed = append(failed, row)
+			unclaimed = append(unclaimed, row)
+			continue
 		}
 
 		newRow := []string{row[0], row[1]}
 		key := strings.Join(newRow, ",")
-		if !existingRows[key] {
-			nonExistingRows = append(nonExistingRows, row)
+		if val, ok := existingRows[key]; ok {
+			status, _ := strconv.Atoi(val)
+			if status < 4 {
+				newRow = append(newRow, val)
+				unclaimed = append(unclaimed, newRow)
+			}
+		} else {
+			failed = append(failed, newRow)
 		}
 	}
 
-	return nonExistingRows, nonClaimedQuestUserData
+	return failed, unclaimed
 }
 
-// WriteNonExistingRowsToFile writes the non-existing rows to a CSV file
-func writeNonExistingRowsToFile(rows [][]string, filename string) error {
+// writeDataToFile writes the non-existing rows to a CSV file
+func writeDataToFile(rows [][]string, filename string) error {
 	file, err := os.Create(filename)
 	if err != nil {
 		return err
